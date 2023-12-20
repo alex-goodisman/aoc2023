3227c2a56b1cbdc90cac89ef81733550efa25105
Alex Goodisman <alex.goodisman@tulip.co>
2023-12-20 17:36:16 -0500

    more scratch work day 20

--- day20/index.js
+++ day20/index.js
@@ -1,105 +1,47 @@
 const { readFileSync } = require('fs');
 const { EOL } = require('os');
-const { inspect } = require('util');
-
-// generalized recursion helper
-function until(seed, step, predicate) {
-    while(!predicate(seed)) {
-        seed = step(seed);
-    }
-    return seed;
-}
 
 const lines = readFileSync('./input.txt').toString().trim().split(EOL);
 
-const states = {};
+let states = lines.map(line => line.split(' -> ')).map(([a,b]) => '&%'.includes(a[0]) ? [a.slice(1), a[0], b.split(', ')] : [a, null, b.split(', ')]).reduce((states, [name, type, down]) => down.reduce((states, d) =>  Object.assign(states, {[d]: Object.assign(states[d] || {down: []}, {state: (!(d in states) || typeof states[d].state === 'object') ? Object.assign(d in states ? states[d].state : {}, {[name]: false}) : states[d].state})}), Object.assign(states, {[name]: Object.assign(states[name] || {state: {}}, {type, down}, type === '%' ? {state: false} : {})})), {});
 
-const reverse = {};
-
-lines.forEach(line => {
-    const [a, b] = line.split(' -> ');
-    const [type, name] = '&%'.includes(a[0]) ? [a[0], a.slice(1)] : [null, a];
-    const down = b.split(', ');
-
-    if (!(name in states)) {
-        states[name] = {state: {}};
-    }
-
-    states[name].type = type;
-    states[name].down = down;
-
-    if (type === '%') {
-        states[name].state = false;
-    }
-
-    down.forEach(d => {
-        if (!(d in states)) {
-            states[d] = {down: [], state: {}};
-        }
-        states[d].state[name] = false;
-
-        if (!(d in reverse)) {
-            reverse[d] = [];
-        }
-        reverse[d].push(name);
-    })
-});
 
 let totalLow = 0;
 let totalHigh = 0;
 
 let presses = 0;
 
-const precursors = Object.fromEntries(reverse['rx'].flatMap(x => reverse[x]).map(x => [x, 0]));
-let precursorProd = 0;
+let precursors = Object.fromEntries(Object.keys(states).filter(k => states[k].down.includes('rx')).flatMap(x => Object.keys(states).filter(k => states[k].down.includes(x))).map(x => [x, 0]));
 
 function pulse() {
+    presses++;
 
-    const q = [[false, 'broadcaster', '']];
+    let q = [[false, 'broadcaster', '']];
 
     while(q.length > 0) {
-        const [high, name, prev] = q.shift();
-        const module = states[name];
+        const [[high, name, prev], ...rest] = q;
 
-        if (high && prev in precursors) {
-            precursors[prev] = precursors[prev] || presses;
-            precursorProd = Object.values(precursors).reduce((a,b) => a * b, 1);
-        }
+        precursors = Object.assign(precursors, (high && prev in precursors) ? {[prev]: precursors[prev] || presses} : {});
 
-        if (high) {
-            totalHigh++;
-        } else {
-            totalLow++;
-        }
 
-        if (module.type === '%') {
-            if (!high) {
-                module.state = !module.state
-                module.down.forEach(d => q.push([module.state, d, name]));
-            }
-        } else if (module.type === '&') {
-            module.state[prev] = high;
-            if (Object.values(module.state).includes(false)) {
-                module.down.forEach(d => q.push([true, d, name]));
-            } else {
-                module.down.forEach(d => q.push([false, d, name]));
-            }
-        } else {
-            // broadcaster
-            module.down.forEach(d => q.push([high, d, name]));
-        }
+        totalHigh += high;
+        totalLow += !high;
+
+        states = Object.assign(states, {[name]: Object.assign(states[name], {state: (states[name].type === '%' && !high) ? (!states[name].state) : (states[name].type === '&') ? (Object.assign(states[name].state, {[prev]: high})) : (states[name].state)})});
+        
+
+        q = rest.concat(((states[name].type === '%') ? (high ? [] : [states[name].state]) : (states[name].type === '&') ? ([Object.values(states[name].state).includes(false)]) : ([high])).flatMap(p => states[name].down.map(d => [p, d, name])));
+        
     }
 }
 
 
 while (presses < 1000) {
-    presses++;
     pulse(false, 'broadcaster');
 }
 console.log(totalLow * totalHigh);
 
-while (precursorProd === 0) {
-    presses++;
+while (Object.values(precursors).reduce((a,b) => a * b, 1) === 0) {
     pulse(false, 'broadcaster');
 }
-console.log(precursorProd);
+console.log(Object.values(precursors).reduce((a,b) => a * b, 1));
